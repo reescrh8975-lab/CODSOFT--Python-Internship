da0c8a8776880acdc0749da4f3e789472715b13f
reescrh8975-lab <reescrh8975@gmail.com>
2026-06-21 16:09:10 +0530

    Implement basic calculator functionality

---
+++ task2_calculator.py
@@ -0,0 +1,12 @@
+a=float(input("First Number: "))
+b=float(input("Second Number: "))
+op=input("Operator (+,-,*,/): ")
+
+if op=="+":
+    print(a+b)
+elif op=="-":
+    print(a-b)
+elif op=="*":
+    print(a*b)
+elif op=="/":
+    print(a/b)
